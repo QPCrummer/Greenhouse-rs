16be1ca23d25570fd60cec09d98cc5aa809262af
HackedOS <hackos@hackos.dev>
2024-11-21 18:19:04 +0530

    fix shit sorry no functions :(

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 #![no_std]
 #![no_main]
 
-use core::ptr::null;
 use core::time::Duration;
-use arduino_hal::{DefaultClock, Delay, I2c, Peripherals, Pins};
-use arduino_hal::adc::channel::Gnd;
-use arduino_hal::hal::port::{PB3, PB4, PD2, PD3, PD4, PD5};
-use arduino_hal::pac::TWI;
-use arduino_hal::port::mode::{Output};
+use arduino_hal::{Delay, I2c, Peripherals};
+use arduino_hal::hal::port::Dynamic;
+use arduino_hal::port::mode::{OpenDrain, Output};
 use arduino_hal::port::Pin;
-use arduino_hal::prelude::_embedded_hal_timer_CountDown;
 use bme680::{Bme680, I2CAddress, IIRFilterSize, OversamplingSetting, PowerMode, SettingsBuilder};
 use lcd1602_driver::command::DataWidth;
 use lcd1602_driver::lcd;
@@ -68,26 +64,13 @@ fn main() -> ! {
     let dp = Peripherals::take().unwrap();
     let twi = dp.TWI;
     let pins = arduino_hal::pins!(dp);
-    let mut components = setup(pins, twi);
-
-
-    loop {
-        arduino_hal::delay_ms(1000);
-    }
-}
-
-fn setup(
-    pins: Pins,
-    twi: TWI,
-) -> (Bme680<I2c, Delay>, Lcd<'static, 'static, ParallelSender<Pin<Output, PD2>, Pin<Output, PD4>, Pin<Output, PD5>, 4>, Delay>) {
+    let mut delayer = Delay::new();
     let i2c = I2c::new(
         twi,
         pins.a4.into_pull_up_input(),
         pins.a5.into_pull_up_input(),
         50000,
     );
-
-    let mut delayer = Delay::new();
 
     // Set up BME680
     let mut dev = Bme680::init(i2c, &mut delayer, I2CAddress::Primary).map_err(|e| {
@@ -114,28 +97,30 @@ fn setup(
         }).unwrap();
 
     // Set up LCD1602
-    let mut sender = ParallelSender::new_4pin(
-        pins.d2.into_output(),
-        pins.d0.into_output(),
-        pins.d3.into_output(),
-        pins.d4.into_output(),
-        pins.d5.into_output(),
-        pins.d6.into_output(),
-        pins.d7.into_output(),
+    let mut sender = ParallelSender::<Pin<Output, Dynamic>,Pin<OpenDrain, Dynamic>,Pin<Output, Dynamic>, 4>::new_4pin(
+        pins.d2.into_output().downgrade(),
+        pins.d0.into_output().downgrade(),
+        pins.d3.into_output().downgrade(),
+        pins.d4.into_opendrain().downgrade(),
+        pins.d5.into_opendrain().downgrade(),
+        pins.d6.into_opendrain().downgrade(),
+        pins.d7.into_opendrain().downgrade(),
         None,
     );
 
-    let mut delayer1 = Delay::new();
     let lcd_config = lcd::Config::default().set_data_width(DataWidth::Bit4);
 
     let lcd = Lcd::new(
         &mut sender,
-        &mut delayer1,
+        &mut delayer,
         lcd_config,
         10,
     );
 
-    (dev, lcd)
+
+    loop {
+        arduino_hal::delay_ms(1000);
+    }
 }
 
 fn get_temperature(bme: &Bme680<I2c, Delay>) {
